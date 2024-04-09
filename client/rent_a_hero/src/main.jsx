31797bc680b1df7697c993885f2f78125738291c
kuta <150553585+qutasi@users.noreply.github.com>
2024-04-09 16:05:20 +0200

    setup react-router-dom

--- client/rent_a_hero/src/main.jsx
+++ client/rent_a_hero/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Welcome from './assets/pages/Welcome.jsx';
+
+const router = createBrowserRouter([{path: '/welcome', element: <Welcome/>}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
+
