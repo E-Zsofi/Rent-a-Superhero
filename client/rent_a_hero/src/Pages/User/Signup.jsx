567b817281ebcb18b2bc651f495437cd1ed6c837
Tamas Szondi <150489253+TamaSzondi@users.noreply.github.com>
2024-04-24 10:15:01 +0200

    userPage featureless

--- client/rent_a_hero/src/Pages/User/Signup.jsx
+++ client/rent_a_hero/src/Pages/User/Signup.jsx
@@ -1,6 +1,5 @@
 import Header from "./Header"
 import { useState, useEffect } from "react";
-import "./User.css"
 
 export default function Signup() {
 
---
+++ client/rent_a_hero/src/Pages/User/UserHomePage.jsx
@@ -0,0 +1,29 @@
+import Header from "./Header.jsx";
+
+
+function HomePage() {
+  return (
+    <div>
+      <div>
+        <Header></Header>
+      </div>
+      <div className="welcome">
+        <h1>Welcome to Rent-a-Hero!</h1>
+        <h2>Your Trusted Superhero Security Service Provider!</h2>
+        <h3>At Rent-a-Hero, we understand that protecting your home and loved ones is your top priority. That's why we offer the most reliable and elite superhero security services in the industry. Our team of skilled superheroes is dedicated to ensuring the safety and security of your residence, providing you with peace of mind and a sense of protection like no other.</h3>
+        <h3>On our Home page, you'll discover the comprehensive range of services we offer to safeguard your home:</h3>
+          <li>Superhero Security Personnel: Our roster includes a diverse array of superheroes, each with unique abilities and powers tailored to meet your specific security needs. From enhanced strength to advanced technology, our superheroes are equipped to handle any threat with efficiency and professionalism.</li>
+          <br></br>
+          <li>Customized Security Solutions: We understand that every home is unique, which is why we provide customized security solutions tailored to your property's layout, location, and vulnerabilities. Whether you need round-the-clock surveillance, perimeter defense, or emergency response services, we've got you covered.</li>
+          <br></br>
+          <li>Cutting-edge Technology: In addition to our superhero personnel, we leverage state-of-the-art security technology to enhance protection levels. From advanced surveillance systems to biometric access control, we employ the latest innovations to fortify your home against intruders and ensure swift response times in case of emergencies.</li>
+          <br></br>
+          <li>24/7 Monitoring and Support: Our team of superhero security experts is available round-the-clock to monitor your home and provide immediate assistance whenever needed. With our proactive approach to security, you can rest assured that we're always one step ahead, keeping you safe and secure at all times.</li>
+          <br></br>
+          <li>Client Testimonials: Hear from satisfied clients who have entrusted their home security to Rent-a-Hero. Their testimonials showcase our commitment to excellence and the unparalleled level of protection we provide.</li>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
